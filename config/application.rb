c9733e83b30da4dbd722c060f2f961e1acdf15e5
hyuraku <yu@hill-wood.com>
2018-08-27 17:53:44 +0900

    apply slim I convert template engine from erb to slim

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,15 @@ module ManageContact
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.javascript      false
+      g.helper          false
+      g.template_engine :haml
+      g.test_framework  false
+    end
+
+    config.time_zone = 'Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
