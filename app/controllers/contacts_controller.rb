c623a6afda24a0080c7bf2e782736751a0a34a07
hyuraku <yu@hill-wood.com>
2018-08-29 18:12:59 +0900

    enable new contact to save in DB

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,29 @@
 class ContactsController < ApplicationController
   def index
-    if params[:group_id].present?
-      @contacts = Contact.where(group_id: params[:group_id]).page(params[:page]).per(7)
-    else
-      @contacts = Contact.page(params[:page]).per(7)
-    end
-    @groups = Group.all
+      if params[:group_id] && !params[:group_id].empty?
+          @contacts = Contact.where(group_id: params[:group_id]).page(params[:page]).per(5)
+      else
+          @contacts = Contact.order(created_at: :desc).page(params[:page]).per(5)
+      end
+  end
+
+  def new
+      @contact = Contact.new
+  end
+
+  def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+          flash[:success] = "Contact was successfully created."
+          redirect_to contacts_path
+      else
+          render 'new'
+      end
+  end
+
+  private
+
+  def contact_params
+      params.require(:contact).permit(:name, :email, :company, :address, :phone, :group_id)
   end
 end
