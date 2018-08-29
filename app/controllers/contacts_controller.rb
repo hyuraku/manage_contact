fb15997317277db9f4e779c9df2da69da700c460
hyuraku <yu@hill-wood.com>
2018-08-29 16:20:54 +0900

    made side bar's selected link active

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
   def index
-    @contacts = Contact.page.per(7)
+    @contacts = Contact.page(params[:page]).per(7)
     @groups = Group.all
   end
 end
