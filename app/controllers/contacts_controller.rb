bb366be277b83b5bc75152847d211cad8e6a8151
hyuraku <yu@hill-wood.com>
2018-08-29 16:29:10 +0900

    solve [rubocop] probrem Rails/Present: Use `params[:group_id].present?` instead of `params[:group_id] && !params[:group_id].empty?`.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
   def index
-    if params[:group_id] && !params[:group_id].empty?
+    if params[:group_id].present?
       @contacts = Contact.where(group_id: params[:group_id]).page(params[:page]).per(7)
     else
       @contacts = Contact.page(params[:page]).per(7)
