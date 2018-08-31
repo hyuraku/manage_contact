8f17b80629da651063d78cdab4810b9e7c060688
hyuraku <yu@hill-wood.com>
2018-08-31 16:23:16 +0900

    DRYed up the contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,5 @@
 class ContactsController < ApplicationController
+  before_action :find_contact,only: [:edit,:update,:destroy]
   def index
       if params[:group_id].present?
           @contacts = Contact.where(group_id: params[:group_id]).page(params[:page]).per(5)
@@ -22,11 +23,10 @@ class ContactsController < ApplicationController
   end
 
   def edit
-    @contact = Contact.find(params[:id])
+
   end
 
   def update
-    @contact = Contact.find(params[:id])
     if @contact.update(contact_params)
       flash[:success] = "Contact was successfully updated."
       redirect_to root_path
@@ -36,12 +36,16 @@ class ContactsController < ApplicationController
   end
 
   def destroy
-    Contact.find(params[:id]).destroy
+    @contact.destroy
     flash[:success] = "Contact was successfully deleted."
     redirect_to root_path
   end
 
   private
+
+  def find_contact
+    @contact = Contact.find(params[:id])
+  end
 
   def contact_params
       params.require(:contact).permit(:name, :email, :company, :address, :phone, :group_id, :avatar)
