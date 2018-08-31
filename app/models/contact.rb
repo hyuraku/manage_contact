e5a1ea682ad7cc7e80384daeab7da232e553eaca
hyuraku <yu@hill-wood.com>
2018-08-31 15:31:28 +0900

    add new feature to upload contact avatar using paperclim gem

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,9 @@ class Contact < ApplicationRecord
   validates :name,:email,:group_id,presence: true
   validates :name,length: {minimum: 4}
 
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/default.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
   def gravatar
     hash = Digest::MD5.hexdigest(email.downcase)
     "https://www.gravatar.com/avatar/#{hash}"
