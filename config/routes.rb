3b0ebaf36f8418d5e71ee46db81c4e3b9b2d5d8b
hyuraku <yu@hill-wood.com>
2018-09-08 12:28:27 +0900

    fixed the current user name

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'dashbord/index'
+
   devise_for :users
   resources :contacts,except: [:show] do
     get 'autocomplete',on: :collection
   end
 
   resources :groups, only: [:create]
+  get '/dashboard' => "dashboard#index"
   root 'contacts#index'
 end
