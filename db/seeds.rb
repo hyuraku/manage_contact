bac83f6eb2d2c99f7dd0bff7623ea96ba6519103
hyuraku <yu@hill-wood.com>
2018-09-10 22:52:35 +0900

    Modified seed modifird the database seed to fill the user_id column in users and groups table

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,36 @@
 
 Contact.destroy_all
 Group.destroy_all
+User.destroy_all
 
-group_ids = []
+user_ids = []
+user_ids << User.create(name: "Tim Holand",email: "dev01@gmail.com",password: "123456").id
+user_ids << User.create(name: "Tom Ford",email: "dev02@gmail.com",password: "123456").id
 
-group_ids <<  Group.create(name: "family").id
-group_ids <<  Group.create(name: "friend").id
-group_ids <<  Group.create(name: "client").id
+p "created #{user_ids.count} group"
+
+group_ids = { user_ids[0] => [], user_ids[1] => []}
+group_ids[user_ids[0]] <<  Group.create(name: "Family",user_id: user_ids[0]).id
+group_ids[user_ids[0]] <<  Group.create(name: "Friend",user_id: user_ids[0]).id
+group_ids[user_ids[1]] <<  Group.create(name: "Client",user_id: user_ids[1]).id
+group_ids[user_ids[1]] <<  Group.create(name: "Company",user_id: user_ids[1]).id
 
 p "created #{group_ids.count} group "
 
 group_count = group_ids.count
-number_of_contacts = 20
+number_of_contacts = 60
 contacts = []
 
 number_of_contacts.times do |n|
+  user_id = user_ids[Random.rand(0...user_ids.count)]
   new_concact = {
     name: Faker::Name.name,
     company: Faker::Company.name,
     email: Faker::Internet.email,
     phone: Faker::PhoneNumber.cell_phone,
     address: "#{Faker::Address.street_address} #{Faker::Address.zip} #{Faker::Address.city}",
-    group_id: group_ids[Random.rand(0...group_count)]
+    group_id: group_ids[user_id][Random.rand(0...group_count)],
+    user_id: user_id
   }
   contacts.push(new_concact)
 end
