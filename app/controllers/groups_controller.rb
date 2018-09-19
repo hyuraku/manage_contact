d0d466f18a79c5512a03476c4f97bc917fc1e7f3
hyuraku <yu@hill-wood.com>
2018-09-19 22:51:44 +0900

    ajaxfy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,5 @@
 class GroupsController < ApplicationController
-  protect_from_forgery :except => [:create]
-
+  protect_from_forgery except: :create
 
   def create
     # @group = Group.new(group_params)
