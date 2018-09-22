024740ea804ccf9baa7b82b4f350384f9168e532
hyuraku <yu@hill-wood.com>
2018-09-22 16:59:55 +0900

    fix file and add new gem

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -2,15 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ContactsController, type: :controller do
   describe "GET index" do
-    # it "assigns @teams" do
-    #   team = Contact.create
-    #   get :index
-    #   expect(assigns(:teams)).to eq([team])
-    # end
     # subject { get :index }
     #
     # it "renders the index template" do
-    #   expect(response).to render_template("index")
+    #  expect(subject).to render_template("contact")
     # end
   end
 end
